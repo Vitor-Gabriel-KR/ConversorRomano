4c535240f95479ebd0f555cdee0481a7247b0bd3
Vitor Gabriel <krosharesheniye@gmail.com>
2025-03-17 01:52:34 -0300

    Bonus Feitos

---
+++ src/components/Converter.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <InputField v-model="inputValue" @input="convertAutomatically" />
+    <br>
+    <button @click="convert">Converter</button>
+    <OutputField :resultValue="result" />
+  </div>
+</template>
+
+<script>
+import InputField from './InputField.vue';
+import OutputField from './OutputField.vue';
+
+export default {
+  components: { InputField, OutputField },
+  data() {
+    return {
+      inputValue: '',
+      result: ''
+    };
+  },
+  methods: {
+    convert() {
+      if (this.isRoman(this.inputValue)) {
+        this.result = this.romanToDecimal(this.inputValue);
+      } else if (this.isDecimal(this.inputValue)) {
+        const decimal = parseInt(this.inputValue, 10);
+        if (decimal > 10000) {
+          this.result = 'Erro: O limite de 10.000 foi atingido.';
+        } else {
+          this.result = this.decimalToRoman(decimal);
+        }
+      } else {
+        this.result = 'Erro: Entrada inválida';
+      }
+    },
+    convertAutomatically() {
+      this.convert();
+    },
+    isRoman(str) {
+      return /^[IVXLCDM]+$/i.test(str);
+    },
+    isDecimal(str) {
+      return /^\d+$/.test(str);
+    },
+    romanToDecimal(roman) {
+      const romanMap = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+      let total = 0;
+      for (let i = 0; i < roman.length; i++) {
+        const current = romanMap[roman[i].toUpperCase()];
+        const next = romanMap[roman[i + 1]?.toUpperCase()];
+        if (next && current < next) {
+          total -= current;
+        } else {
+          total += current;
+        }
+      }
+      return total || 'Erro: Entrada inválida';
+    },
+    decimalToRoman(decimal) {
+      const val = [
+        1000, 900, 500, 400,
+        100, 90, 50, 40,
+        10, 9, 5, 4,
+        1
+      ];
+      const syms = [
+        "M", "CM", "D", "CD",
+        "C", "XC", "L", "XL",
+        "X", "IX", "V", "IV",
+        "I"
+      ];
+      let num = parseInt(decimal, 10);
+      if (isNaN(num)) {
+        return 'Erro: Entrada inválida';
+      }
+      let result = '';
+      for (let i = 0; i < val.length; i++) {
+        while (num >= val[i]) {
+          result += syms[i];
+          num -= val[i];
+        }
+      }
+      return result;
+    }
+  }
+};
+</script>
---
+++ src/components/InputField.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <label for="input">Digite um Número Romano ou Decimal:</label>
+    <input
+      id="input"
+      type="text"
+      v-model="inputValue"
+      @input="updateValue"
+      placeholder="Digite um número romano ou decimal..."
+    />
+  </div>
+  <label class="hints">Números romanos podem ser: I, V, X, L, C, D, M</label>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: String
+  },
+  computed: {
+    inputValue: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value.toUpperCase());
+      }
+    }
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('update:modelValue', event.target.value.toUpperCase());
+      this.$emit('input', event);
+    }
+  }
+};
+</script>
+
+<style scoped>
+input {
+  padding: 8px;
+  font-size: 13px;
+  width: 200px;
+  text-transform: uppercase;
+}
+.hints {
+  font-size: 12px;
+  color: #888;
+  margin-top: 4px;
+  display: block;
+}
+</style>
